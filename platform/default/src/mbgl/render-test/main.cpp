411bd6d80e8397dbc8b839338132b7265679d46b
mwilsnd <mwilsnd@gmail.com>
2023-06-27 15:41:53 -0500

    De-dup default, darwin and macOS platform locations

--- platform/default/src/mbgl/render-test/main.cpp
+++ platform/default/src/mbgl/render-test/main.cpp
@@ -1,5 +1,13 @@
 #include <mbgl/render_test.hpp>
 
-int main(int argc, char *argv[]) {
-    return mbgl::runRenderTests(argc, argv, []() {});
+int main(int argc, char *argv[])
+#ifdef TARGET_OS_IPHONE
+try {
+    return mbgl::runRenderTests(argc, argv);
+} catch (std::exception const& e) {
+    std::cerr << "Caught an exception while running tests\n" << e.what() << '\n';
+    return 1;
+#else
+    return mbgl::runRenderTests(argc, argv);
+#endif
 }
