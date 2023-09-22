75f378c9fb02349ac5b8d4672f28c17cb7cc9b08
mwilsnd <mwilsnd@gmail.com>
2023-09-22 12:36:18 -0400

    Use libicu for UTF conversion

--- platform/default/src/mbgl/util/utf.cpp
+++ platform/default/src/mbgl/util/utf.cpp
@@ -1,16 +1,18 @@
 #include <mbgl/util/utf.hpp>
-
-#include <boost/locale/encoding_utf.hpp>
+#include <unicode/unistr.h>
 
 namespace mbgl {
 namespace util {
 
 std::u16string convertUTF8ToUTF16(const std::string& str) {
-    return boost::locale::conv::utf_to_utf<char16_t>(str);
+    auto ustr = icu::UnicodeString::fromUTF8(icu::StringPiece(str));
+    return std::u16string(ustr.getBuffer(), static_cast<size_t>(ustr.length()));
 }
 
 std::string convertUTF16ToUTF8(const std::u16string& str) {
-    return boost::locale::conv::utf_to_utf<char>(str);
+    std::string ustr;
+    icu::UnicodeString(true /* isTerminated */, str.c_str(), static_cast<int32_t>(str.length())).toUTF8String(ustr);
+    return ustr;
 }
 
 } // namespace util
